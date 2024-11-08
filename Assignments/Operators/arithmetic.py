ba2ab8e8a6d7e73e7705ce0eec1a522bd16c723f
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 14:22:53 +0530

    Create arithmetic.py

---
+++ Assignments/Operators/arithmetic.py
@@ -0,0 +1,16 @@
+#Taking input from user
+num1=int(input("Enter first number : "))
+num2=int(input("Enter second number : "))
+
+add=num1+num2   #addition
+sub=num1-num2   #subtraction
+mul=num1*num2   #multiplication
+div=num1/num2   #division returns quotient
+mod=num1%num2   #modulus operator returns remainder
+
+#Printing output
+print("\nThe Addition is : ",add)
+print("The Subtraction is : ",sub)
+print("The Multiplication is : ",mul)
+print("The Division is : ",div)
+print("The Modulus is : ",mod)
