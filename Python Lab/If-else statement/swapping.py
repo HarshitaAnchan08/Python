eb3231ba4707099b6acb00db6cadf07dad4e9fd4
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 10:05:02 +0530

    Update swapping.py

--- Python Lab/If-else statement/swapping.py
+++ Python Lab/If-else statement/swapping.py
@@ -1,1 +1,21 @@
+#Swapping of two numbers using third variable
+
+#Input numbers
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print("\nBefore Swapping : ")
+print("\na = ",a)
+print("\nb = ",b)
+
+#logical section
+temp=a
+a=b
+b=temp
+
+#printing after swapping of numbers
+print("\nAfter Swapping : ")
+print("\na = ",a)
+print("\nb = ",a)
+
+
 
