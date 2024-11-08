ff9b0062889744be96fdff3995eceb2d6b80d698
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 14:43:04 +0530

    Create Rectangle.py

---
+++ Assignments/Operators/Rectangle.py
@@ -0,0 +1,12 @@
+#Taking input from user
+length=float(input("Enter length : "))
+breadth=float(input("Enter breadth :" ))
+
+#formula
+area=length*breadth
+peri=2*(length+breadth)
+
+#Printing result
+print("Area of Rectangle : ",area)
+print("Perimeter of Rectangle : ",peri)
+
