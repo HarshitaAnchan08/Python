01d4f036162505b641dd262e80471dd534229b73
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 14:37:28 +0530

    Create Circle.py

---
+++ Assignments/Operators/Circle.py
@@ -0,0 +1,13 @@
+#Taking input from user
+rad=float(input("Enter a radius : "))
+
+#Initialize pie variable
+pie=3.14
+
+#formula
+area=pie*rad*rad
+peri=2*pie*rad
+
+#Printing result
+print("Area of circle : ",area)
+print("Perimeter of Circle : ",peri)
