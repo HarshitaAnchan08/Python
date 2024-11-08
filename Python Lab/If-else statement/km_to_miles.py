e17d6adbeb9a869530208eccfa2e6a587e10cd7f
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 09:58:47 +0530

    Add files via upload

---
+++ Python Lab/If-else statement/km_to_miles.py
@@ -0,0 +1,11 @@
+#Program to convert Kilometers to Miles
+
+#Taking input from user
+kms=float(input("Enter Kilometers : "))
+
+#Converting kilometers to miles
+#logical section
+miles=kms*0.62137119
+
+#displaying output
+print("The ",kms," in miles are ",miles)
