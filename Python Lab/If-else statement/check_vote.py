bcab702464b719704b19a997080fae2521e334d8
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 09:59:44 +0530

    Add files via upload

---
+++ Python Lab/If-else statement/check_vote.py
@@ -0,0 +1,10 @@
+#To check if the user is Eligible to vote
+
+#Taking age as input from user
+age=int(input("Enter your age : "))
+
+#conditionl Logic
+if (age>=18):
+    print("You are eligible to vote...")
+else:
+    print("You are not eligible to vote...")
---
+++ Python Lab/If-else statement/simple_interest.py
@@ -0,0 +1,14 @@
+#Finding simple Interest
+
+#Given Values from the question
+principle=200
+rate=5
+time=5
+
+#logical section
+#simple interest formula
+
+simple_interest=(principle * rate *time)/100
+
+#displaying output
+print("The simple interest is : ",simple_interest)
