5f14efb00722a03b2a73397e29e60fb0fdd43804
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 14:40:22 +0530

    Create Square.py

---
+++ Assignments/Operators/Square.py
@@ -0,0 +1,10 @@
+#Taking input from user
+side=float(input("Enter a side : "))
+
+#formula
+area=side*side
+peri=4*side
+
+#Printing result
+print("Area of Square : ",area)
+print("Perimeter of Square : ",peri)
