12e3d5ed797d77ca3f463a7a32f2189b06d23ede
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-08 14:30:14 +0530

    Create EvenOdd.py

---
+++ Assignments/Conditional Statements/EvenOdd.py
@@ -0,0 +1,9 @@
+#Taking input from user
+num=int(input("Enter a number : "))
+
+#if-else statement
+#if the number is fully divisible by 2 and returns 0 then it is even number else odd number
+if (num%2==0):
+    print(num," is even number.")
+else:
+    print(num," is odd number.")
