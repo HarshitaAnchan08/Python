2eb7f2a153b838b4cb28e0d0aa93a4882850feb6
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-10 00:49:58 +0530

    Create Positive_negative_zero.py

---
+++ Assignments/Conditional Statements/Positive_negative_zero.py
@@ -0,0 +1,12 @@
+#Program to check number is positive, negative or zero
+
+#Take user input
+num=int(input("Enter a number : "))
+
+#Logic conditions
+if (num>0):
+    print(num," is Positive number.")
+elif (num==0):
+    print(num, "is Zero.")
+else:
+    print(num,"is Negative number.")
