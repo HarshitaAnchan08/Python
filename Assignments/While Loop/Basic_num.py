d4dcf8379af5400674dff0c1f91b21a342271d21
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-09 23:38:45 +0530

    Create Basic_num.py

---
+++ Assignments/While Loop/Basic_num.py
@@ -0,0 +1,8 @@
+#Initialize a variable num 
+num=1
+
+#While loop
+#Loop wil run until the num is equal to 10
+while num<=10:
+    print(num)   #Printing num after each increment
+    num+=1       #incrementing by 1    num=num+1
