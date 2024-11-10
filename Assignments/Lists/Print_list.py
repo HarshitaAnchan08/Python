1187a3a52663f5743ad738acf5ec2482c90600b6
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-10 13:58:12 +0530

    Create Print_list.py

---
+++ Assignments/Lists/Print_list.py
@@ -0,0 +1,16 @@
+#Program to create list, and print type and contents
+
+#create list
+#list contain combination of data and it is mutable type
+list1=[1,33,4,89,"apple",'A',78.8,"cherry"]
+
+#Printing type ofobject 
+print(type(list1))
+
+#Printing list
+print("The list is : ",list1)
+
+#Prining individul elements in a list
+print("Each elements in a list are : ")
+for i in list1:
+    print(i)
