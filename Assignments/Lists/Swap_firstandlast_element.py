4cc0a920a14868074bd24500ff7ad23ff66b5866
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-11-12 16:50:42 +0530

    Create Swap_firstandlast_element.py

---
+++ Assignments/Lists/Swap_firstandlast_element.py
@@ -0,0 +1,13 @@
+#Program to Swap first and last elements in a list
+
+#Create a list
+list1=[23,65,67,87,90]
+
+#Printing Result
+print("The list before Swapping : ",list1)
+
+#Interchanging first and last element
+list1[0],list1[-1]=list1[-1],list1[0]
+
+#Printing Result
+print("After Swapping first and last elements in a list is : ",list1)
