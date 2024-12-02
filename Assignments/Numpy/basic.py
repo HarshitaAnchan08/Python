89f134c223abd79554e44655de1b99da2b370d17
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-12-02 17:51:44 +0530

    Create basic.py

---
+++ Assignments/Numpy/basic.py
@@ -0,0 +1,20 @@
+#Program to calculate addition and multiplication of two arrays
+
+#importing numpy module
+import numpy as np
+
+#Define arr1
+arr1=np.array([1,2,3])
+print(arr1)
+
+#Define arr2
+arr2=np.array([4,5,6])
+print(arr2)
+
+#addition
+add_arr=arr1+arr2
+print("The addition is :",add_arr)
+
+#multipliction
+mul_arr=arr1*arr2
+print("The multiplication is :",mul_arr)
