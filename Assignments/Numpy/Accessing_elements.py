7792a6b347b140a0d954167410c088a40f76dc7b
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-12-02 18:00:18 +0530

    Create Accessing_elements.py

---
+++ Assignments/Numpy/Accessing_elements.py
@@ -0,0 +1,14 @@
+#Accessing array Elements
+
+#Importing numpy module
+import numpy as np
+
+#define array
+arr=np.array([1,2,3,4,5])
+print("Array :",arr)
+
+#printing element at 0th index
+print("Oth index element :",arr[0])
+
+#printing element at 2nd index
+print("2nd index element :",arr[2])
