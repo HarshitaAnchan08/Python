c0d05a606c87f562c48a338571c53b8bf5d79004
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-12-23 17:24:14 +0530

    Create IN_operator.py

---
+++ Assignments/Numpy/IN_operator.py
@@ -0,0 +1,17 @@
+#Importing numpy module
+import numpy as np
+
+#create a array
+n_array=np.array([[4,5.5,7,6.9,10],
+                  [7.1,5.3,40,8.8,1],
+                  [4.4,9.3,6,2.2,11],
+                  [7.1,4,5,9,10.5]])
+
+#printing array
+print("Given array: ")
+print(n_array)
+
+#Checking whether specific values are present in "m_array" or not
+print(2.14 in n_array)
+print(5.28 in n_array)
+print(8.5 in n_array)
