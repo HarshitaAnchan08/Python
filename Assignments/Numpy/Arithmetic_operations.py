9b2edc4f99322a41228a6c1d28731a407e03409e
Harshita Anchan <harshitaanchan4568@gmail.com>
2024-12-31 16:22:43 +0530

    Create Arithmetic_operations.py

---
+++ Assignments/Numpy/Arithmetic_operations.py
@@ -0,0 +1,23 @@
+#Arithmethic operations
+
+#import numpy module
+import numpy as np
+
+#creating two arrays- arr1 and arr2
+arr1=np.array([1,2,3,7])
+arr2=np.array([4,6,5,3])
+
+#addition
+add_arr=arr1+arr2
+#subtraction
+sub_arr=arr1-arr2
+#multiplication
+multi_arr=arr1*arr2
+#division
+div_arr=arr1/arr2
+
+#print results
+print("The addition is : ",add_arr)
+print("The subtraction is : ",sub_arr)
+print("The multiplication is : ",multi_arr)
+print("The division is : ",div_arr)
