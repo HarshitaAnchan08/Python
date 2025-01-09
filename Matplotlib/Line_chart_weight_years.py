d55ca913649b2bd4ec42a9c62fbac654ab128527
Harshita Anchan <harshitaanchan4568@gmail.com>
2025-01-09 17:02:46 +0530

    Create Line_chart_weight_years.py

---
+++ Matplotlib/Line_chart_weight_years.py
@@ -0,0 +1,8 @@
+#Data Analysis of the weight of a person according to the each year
+
+years = [2000 + x for x in range(10)]
+weight = [80,34,78,67,78,38,29,67,67,67]
+
+plt.plot(years,weight,c="g",lw=3,linestyle="-.")
+
+plt.show()
