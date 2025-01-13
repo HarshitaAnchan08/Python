f4795600680ac8b15eb655da5b0c0cb91a35c41f
Harshita Anchan <harshitaanchan4568@gmail.com>
2025-01-13 15:00:27 +0530

    Create line_plot.py

---
+++ Matplotlib/line_plot.py
@@ -0,0 +1,12 @@
+#creating line using x points and y points
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#x and y coordinate
+x=np.array([0,10])
+y=np.array([0,125])
+
+#plotting
+plt.plot(x,y)
+plt.show()
